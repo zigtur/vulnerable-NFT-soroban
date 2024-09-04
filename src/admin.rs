193924a9c82c54a062b6c4b0cc1f786f1245a20f
zigtur <zigtur@protonmail.com>
2024-09-04 14:57:24 +0200

    Soroban - release

--- src/admin.rs
+++ src/admin.rs
@@ -4,12 +4,12 @@ use crate::errors::Error;
 
 pub fn has_administrator(env: &Env) -> bool {
     let key = DataKey::Admin;
-    env.storage().instance().has(&key)
+    env.storage().temporary().has(&key)
 }
 
 pub fn read_administrator(env: &Env) -> Address {
     let key = DataKey::Admin;
-    match env.storage().instance().get::<DataKey, Address>(&key) {
+    match env.storage().temporary().get::<DataKey, Address>(&key) {
         Some(data) => data,
         None => panic_with_error!(env, Error::NotFound),
     }
@@ -17,5 +17,5 @@ pub fn read_administrator(env: &Env) -> Address {
 
 pub fn write_administrator(env: &Env, id: &Address) {
     let key = DataKey::Admin;
-    env.storage().instance().set(&key, id);
+    env.storage().temporary().set(&key, id);
 }
